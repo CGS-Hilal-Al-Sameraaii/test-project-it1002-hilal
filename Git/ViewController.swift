190928416f9007ad5d0ab5a9d0b4f9d529f8425d
Hilal Alsameraaii <hilalalsameraaii@b00pler.local>
2017-05-22 10:11:22 +1000

    V2

--- Git/ViewController.swift
+++ Git/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        label.text = "Hello World"
+        label.text = "Hello"
     }
 
     override func didReceiveMemoryWarning() {
