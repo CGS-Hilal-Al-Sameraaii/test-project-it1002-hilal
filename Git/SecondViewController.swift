2c4b146fcbd381b7a6b6642c879ae0eaaeee7bfb
Hilal Alsameraaii <hilal.al-sameraaii@cgs.act.edu.au>
2017-06-19 10:30:54 +1000

    New update second view

--- Git/SecondViewController.swift
+++ Git/SecondViewController.swift
@@ -16,6 +16,7 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Okay")
+        print("New Updaetz")
     }
     
     override func didReceiveMemoryWarning() {
