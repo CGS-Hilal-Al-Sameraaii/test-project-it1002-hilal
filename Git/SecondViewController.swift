da8a1950c5310e6ba038ffebae28289fbb24ab65
Hilal Alsameraaii <hilal.al-sameraaii@cgs.act.edu.au>
2017-06-19 10:26:40 +1000

    New view controller

---
+++ Git/SecondViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Git
+//
+//  Created by Hilal Alsameraaii on 19/6/17.
+//  Copyright © 2017 Hilal Al-Sameraaii. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Okay")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
